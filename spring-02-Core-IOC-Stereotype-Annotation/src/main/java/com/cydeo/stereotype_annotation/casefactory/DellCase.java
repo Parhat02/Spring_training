f991707b0d03d5bc82320ed1a0a1e75edd8ae302
Ferhat <parhat002@gmail.com>
2024-03-19 21:16:01 +0300

    stereotype-Annotation

---
+++ spring-02-Core-IOC-Stereotype-Annotation/src/main/java/com/cydeo/stereotype_annotation/casefactory/DellCase.java
@@ -0,0 +1,20 @@
+package com.cydeo.stereotype_annotation.casefactory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class DellCase extends Case {
+
+    public DellCase() {
+        super("2208","Dell","240");
+    }
+
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
