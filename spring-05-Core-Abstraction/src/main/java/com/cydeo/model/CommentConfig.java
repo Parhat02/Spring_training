b1e71d59e533a14018c8369d655395c029976bf6
Ferhat <parhat002@gmail.com>
2024-03-20 22:51:55 +0300

    Abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/model/CommentConfig.java
@@ -0,0 +1,10 @@
+package com.cydeo.model;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@ComponentScan(basePackages = "com.cydeo")
+@ComponentScan(basePackages = {"com.cydeo.proxy", "com.cydeo.repository", "com.cydeo.service"})
+public class CommentConfig {
+}
