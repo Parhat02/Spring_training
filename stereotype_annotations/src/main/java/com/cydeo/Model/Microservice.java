15e6b4eac2047bf32ecc2cc7d0688fe1629bd842
Ferhat <parhat002@gmail.com>
2024-03-25 00:30:13 +0300

    SpringReview

--- stereotype_annotations/src/main/java/com/cydeo/Model/Microservice.java
+++ stereotype_annotations/src/main/java/com/cydeo/Model/Microservice.java
@@ -1,15 +1,19 @@
 package com.cydeo.Model;
 
+import lombok.Data;
+import lombok.NonNull;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class Microservice {
 
-    private final ExtraHours extraHours;
+    @NonNull
+    private ExtraHours extraHours;
 
-    public Microservice(ExtraHours extraHours) {
-        this.extraHours = extraHours;
-    }
+//    public Microservice(ExtraHours extraHours) {
+//        this.extraHours = extraHours;
+//    }
 
     public void getTotalHours(){
         System.out.println("Total hours : " + (25+extraHours.getHours()));
