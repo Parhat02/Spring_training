7f6dd4d3d86263c1b759d7f3b9f9f47fa1c4296a
Ferhat <parhat002@gmail.com>
2024-04-29 20:16:49 +0300

    spring-data

---
+++ spring-11-Data-TableCreattion/src/main/java/com/cydeo/Spring11DataTableCreattionApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11DataTableCreattionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring11DataTableCreattionApplication.class, args);
+    }
+
+}
