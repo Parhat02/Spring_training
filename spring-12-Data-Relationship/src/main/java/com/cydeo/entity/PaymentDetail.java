ce0976000fe29eeb5bc4be82178014ae09e021bb
Ferhat <parhat002@gmail.com>
2024-05-01 14:01:37 +0300

    Table-Relationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -23,6 +23,9 @@ public class PaymentDetail {
     @Column(columnDefinition = "DATE")
     private LocalDate paymentDate;
 
+    @OneToOne(mappedBy = "paymentDetail")
+    private Payment payment;
+
     public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate paymentDate) {
         this.merchantPayoutAmount = merchantPayoutAmount;
         this.commissionAmount = commissionAmount;
