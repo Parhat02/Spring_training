97f65dbdece86191af71e8b688bb195d1ec27a41
Ferhat <parhat002@gmail.com>
2024-06-29 01:00:31 +0300

    RestSecurityKeycloak

---
+++ spring-20-REST-Security-Keycloak-OrganizationsApp/src/main/java/com/cydeo/Spring20RestSecurityKeycloakOrganizationsAppApplication.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring20RestSecurityKeycloakOrganizationsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring20RestSecurityKeycloakOrganizationsAppApplication.class, args);
+    }
+
+}
+
+/*
+{
+        "id": 1,
+        "name": "ABC Limited",
+        "contactName": "tom",
+        "contactEmail": "tom@gmail.com",
+        "contactPhone": "2345654433"
+        }
+
+ */
